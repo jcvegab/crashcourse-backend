5f0db83102f39288a48a679b4109954adb65df51
Joseph Vega <jcvb0206@gmail.com>
2021-02-26 14:11:18 -0500

    Add login process to GraphQL

--- backend/schema.py
+++ backend/schema.py
@@ -7,6 +7,8 @@ from graphql_auth.schema import UserQuery, MeQuery
 
 class AuthMutation(graphene.ObjectType):
     register = mutations.Register.Field()
+    verify_account = mutations.VerifyAccount.Field()
+    token_auth = mutations.ObtainJSONWebToken.Field()
 
 
 class Query(UserQuery, MeQuery, graphene.ObjectType):
@@ -15,9 +17,6 @@ class Query(UserQuery, MeQuery, graphene.ObjectType):
 
 class Mutation(AuthMutation, graphene.ObjectType):
     pass
-    # token_auth = graphql_jwt.ObtainJSONWebToken.Field()
-    # verify_token = graphql_jwt.Verify.Field()
-    # refresh_token = graphql_jwt.Refresh.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
