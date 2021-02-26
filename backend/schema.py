a0f61ba6586570c017f03b9e8d760984ea8641e8
Joseph Vega <jcvb0206@gmail.com>
2021-02-26 14:16:25 -0500

    Update of users account with GraphQL

--- backend/schema.py
+++ backend/schema.py
@@ -9,6 +9,7 @@ class AuthMutation(graphene.ObjectType):
     register = mutations.Register.Field()
     verify_account = mutations.VerifyAccount.Field()
     token_auth = mutations.ObtainJSONWebToken.Field()
+    update_account = mutations.UpdateAccount.Field()
 
 
 class Query(UserQuery, MeQuery, graphene.ObjectType):
