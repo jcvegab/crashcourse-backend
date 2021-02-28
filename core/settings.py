4ef2ba8bb646e988d1c67e674fd898f79477858e
Joseph Vega <jcvb0206@gmail.com>
2021-02-27 20:23:02 -0500

    Set Debug false in Django

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "^@ynff#06j#ts)niigj4t95dh#j@)1u*1ni%jwb5j8+nr@92b^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
